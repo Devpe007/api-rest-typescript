351be96f39d420abb73c2ce0214f1636e6c8528e
Devpe007 <anddev007pe@gmail.com>
2022-11-29 08:43:37 -0300

    configurate data-source typeorm

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+import { AppDataSource } from './data-source';
+
+AppDataSource.initialize().then(() => {
+  const app = express();
+
+  app.use(express.json());
+
+  app.get('/', (request, response) => response.json('tudo certo'));
+
+  return app.listen(3333, () => console.log('Server is running on port 3333'));
+});
